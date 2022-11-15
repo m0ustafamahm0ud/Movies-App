898a303f28823b511d8c529a56a90286fd1798e6
Moustafa Mahmoud <m0ustafamahm0ud@yahoo.com>
2022-11-15 23:50:05 +0200

    make another changes again and again

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   ServiceLocator().init();
   // make some changes to the code
   // make another changes again
+  // make another changes again and again
   runApp(const MyApp());
 }
 
